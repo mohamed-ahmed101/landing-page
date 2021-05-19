c16e987d50c7e9a78ab119652dde3f5a85c77e19
Moahmed Ahmed <47562078+mohamed-ahmed101@users.noreply.github.com>
2021-05-19 23:42:45 +0200

    Update app.js
    add new section

--- js/app.js
+++ js/app.js
@@ -104,6 +104,23 @@ const content = [{
     porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.`
     ]
 
+},{
+    anchorText: "section4",
+    anchorCalss: "menu__link",
+    sectionId: "section4",
+    sectionDataNav: "Section 4",
+    sectionDivCalss: "landing__container",
+    sectionParagraphs: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra
+    dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus
+    imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget
+    bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet
+    elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo
+    nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie
+    semper in tellus. Sed congue et odio sed euismod.`, `Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel
+    luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur
+    porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.`
+    ]
+
 }]
 var navBarList = document.querySelector('#navbar__list');
 let main = document.querySelector('main');
